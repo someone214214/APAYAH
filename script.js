d134c5cadb3f28feaad02491f44ee503b534518f
someone214214 <drewoii85@gmail.com>
2026-06-12 01:32:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,71 @@
 const music = document.getElementById("music");
 
 function showMenu(){
-    document.getElementById("welcome").classList.remove("active");
-    document.getElementById("menu").classList.add("active");
+
+    document
+    .getElementById("welcome")
+    .classList.remove("active");
+
+    document
+    .getElementById("menu")
+    .classList.add("active");
 }
 
 function openGallery(){
-    document.getElementById("menu").classList.remove("active");
-    document.getElementById("gallery").classList.add("active");
+
+    document
+    .getElementById("menu")
+    .classList.remove("active");
+
+    document
+    .getElementById("gallery")
+    .classList.add("active");
 
     music.play();
-
-    document.getElementById("gallery").scrollIntoView({
-        behavior:"smooth"
-    });
 }
 
 function openNote(){
-    document.getElementById("menu").classList.remove("active");
-    document.getElementById("note").classList.add("active");
+
+    document
+    .getElementById("menu")
+    .classList.remove("active");
+
+    document
+    .getElementById("note")
+    .classList.add("active");
 
     music.play();
-
-    document.getElementById("note").scrollIntoView({
-        behavior:"smooth"
-    });
 }
 
-function createFlower(){
-    const flower = document.createElement("div");
+/* BUNGA JATUH */
 
-    flower.innerHTML = "🌹";
+function createFlower(){
+
+    const flower =
+    document.createElement("div");
+
     flower.classList.add("flower");
 
-    flower.style.left = Math.random()*100 + "vw";
-    flower.style.fontSize = (20 + Math.random()*20) + "px";
+    const flowers = [
+        "🌹",
+        "🌺",
+        "🌸",
+        "💖"
+    ];
+
+    flower.innerHTML =
+    flowers[
+        Math.floor(
+            Math.random() * flowers.length
+        )
+    ];
+
+    flower.style.left =
+    Math.random()*100 + "vw";
+
+    flower.style.fontSize =
+    (20 + Math.random()*20) + "px";
+
     flower.style.animationDuration =
     (5 + Math.random()*5) + "s";
 
